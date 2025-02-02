14b8a7cfaed6bc6594b8bc58df4867ce78893483
Aizhan4ik <aijan18112005@gmail.com>
2025-02-02 20:39:39 +0600

    Create laptop.py

---
+++ laptop.py
@@ -0,0 +1,14 @@
+#Aizhan Mamatkalykova
+
+laptop = { "brand": "dell", "model": "alienware", "year": 2010 }
+
+# 1
+print("Brand:", laptop["brand"])
+
+# 2
+laptop["home"] = True
+print("Updated dictionary:", laptop)
+
+# 3
+laptop["year"] = 2019
+print("Modified dictionary:", laptop)
